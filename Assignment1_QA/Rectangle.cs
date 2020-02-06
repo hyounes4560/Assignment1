e7077872744e4a2a93162ecb6e88cdfcdd39bfae
hyounes4560 <hyounes4560@conestogac.on.ca>
2020-02-06 15:49:07 -0500

    Built Rectangle Class Properties and Methods

--- Assignment1_QA/Rectangle.cs
+++ Assignment1_QA/Rectangle.cs
@@ -8,5 +8,71 @@ namespace Assignment1_QA
 {
     class Rectangle
     {
+
+        private int length;
+        private int width;
+
+        //public int Length { get => length; set => length = value; }
+        //public int Width { get => width; set => width = value; }
+
+        public Rectangle() {
+
+            this.length = 1;
+
+            this.width = 1;
+        }
+
+        public Rectangle(int lengthInput, int widthInput)
+        {
+
+            this.length = lengthInput;
+            this.width = widthInput; 
+
+        }
+
+        public int GetLength()
+        {
+
+            return this.length; 
+
+
+        }
+        public int SetLength(int length)
+        {
+
+            this.length = length;
+            return length; 
+
+        }
+        public int GetWidth()
+        {
+            return this.width;
+
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return width; 
+
+
+        }
+        public int GetPerimeter()
+        {
+            int perimeter = 2 * (length + width);
+            return perimeter;
+
+        }
+        public int GetArea()
+        {
+
+            int area = length * width;
+            return area;
+
+        }
+
+
+
+
+
     }
 }
