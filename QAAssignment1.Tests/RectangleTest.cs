66591505b5759b668246346acfa2c04155cf37ba
hyounes4560 <hyounes4560@conestogac.on.ca>
2020-02-06 23:40:33 -0500

    Containing the completed unit tests

--- QAAssignment1.Tests/RectangleTest.cs
+++ QAAssignment1.Tests/RectangleTest.cs
@@ -19,10 +19,63 @@ namespace QAAssignment1.Tests
             [SetUp]
             public void Init()
             {
-               
+                rectangle = new Rectangle();
             }
 
+            public void GetLength_expectedLength1()
+            {
+            
+
+            int actualLength = rectangle.GetLength();
+            Assert.AreEqual(6, actualLength);
+            
+            }
+
+        public void SetLength_inputLength4_actualLength4()
+        {
+
+            int inputLength = 4; 
+            int actualLength = rectangle.SetLength(inputLength);
+            Assert.AreEqual(4, actualLength);
 
 
         }
+
+        public void GetWidth_actualWidth1()
+        {
+            
+            int actualWidth = rectangle.GetWidth();
+            Assert.AreEqual(1, actualWidth);
+
+        }
+
+        public void SetWidth_inputWidth3_actualWidth3()
+        {
+
+            int inputWidth = 3;
+            int actualWidth = rectangle.SetLength(inputWidth);
+            Assert.AreEqual(3, actualWidth);
+
+        }
+
+        public void GetPerimeter_inputLength4_inputWidth3()
+        {
+                        
+            int actualPerimeter= rectangle.GetPerimeter();
+            Assert.AreEqual(14, actualPerimeter);
+
+        }
+
+        public void GetArea_inputLength4_inputWidth3()
+        {
+
+            int actualArea = rectangle.GetArea();
+            Assert.AreEqual(12, actualArea);
+
+
+        }
+
+
+
+    }
     }
