bcb1c3008f5861f2ce23c8273a5f6d08126d972f
hyounes4560 <hyounes4560@conestogac.on.ca>
2020-02-07 11:56:25 -0500

    Adding more tests to the test class

--- QAAssignment1.Tests/RectangleTest.cs
+++ QAAssignment1.Tests/RectangleTest.cs
@@ -25,26 +25,50 @@ namespace QAAssignment1.Tests
         [Test]
         public void GetLength__defaultLength1_actualLength1()
         {
-
+            //default value
+            rectangle = new Rectangle();
             int actualLength = rectangle.GetLength();
             Assert.AreEqual(1, actualLength);
 
         }
 
         [Test]
+        public void GetLength__InputLength2_actualLength2()
+        {
+            int inputLength = 2;
+            int inputWidth = 3;
+            rectangle = new Rectangle(inputLength, inputWidth);
+            int actualLength = rectangle.GetLength();
+            Assert.AreEqual(2, actualLength);
+
+        }
+
+        [Test]
         public void GetWidth__defaultWidth1_actualWidth1()
         {
-
+            
+            rectangle = new Rectangle();
             int actualWidth = rectangle.GetWidth();
             Assert.AreEqual(1, actualWidth);
 
         }
 
         [Test]
+        public void GetWidth__inputWidth2_actualWidth2()
+        {
+            int inputLength = 5;
+            int inputWidth = 2;
+            Rectangle rectangle = new Rectangle(inputLength, inputWidth);
+            int actualWidth = rectangle.GetWidth();
+            Assert.AreEqual(2, actualWidth);
+
+        }
+        [Test]
         public void SetLength_inputLength4_actualLength4()
         {
 
             int inputLength = 4;
+            rectangle = new Rectangle();
             int actualLength = rectangle.SetLength(inputLength);
             Assert.AreEqual(4, actualLength);
 
@@ -54,7 +78,7 @@ namespace QAAssignment1.Tests
         [Test]
         public void SetWidth_inputWidth3_actualWidth3()
         {
-
+            rectangle = new Rectangle();
             int inputWidth = 3;
             int actualWidth = rectangle.SetLength(inputWidth);
             Assert.AreEqual(3, actualWidth);
@@ -62,20 +86,25 @@ namespace QAAssignment1.Tests
         }
 
         [Test]
-        public void GetPerimeter_inputLength1_inputWidth1()
+        public void GetPerimeter_inputLength4_inputWidth3()
         {
 
+            int inputLength = 4;
+            int inputWidth = 3;
+            Rectangle rectangle = new Rectangle(inputLength, inputWidth);
             int actualPerimeter = rectangle.GetPerimeter();
-            Assert.AreEqual(4, actualPerimeter);
+            Assert.AreEqual(14, actualPerimeter);
 
         }
 
         [Test]
-        public void GetArea_inputLength1_inputWidth1()
+        public void GetArea_inputLength4_inputWidth3()
         {
-
+            int inputLength = 4;
+            int inputWidth = 3;
+            Rectangle rectangle = new Rectangle(inputLength, inputWidth);
             int actualArea = rectangle.GetArea();
-            Assert.AreEqual(1, actualArea);
+            Assert.AreEqual(12, actualArea);
 
         }
 
